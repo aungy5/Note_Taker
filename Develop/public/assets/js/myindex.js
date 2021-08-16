784fc761b65780cd1649a440053a18d9b9f861fd
Andrew Ungvary <aungvary5@gmail.com>
2021-08-16 15:53:21 -0400

    adding node_modules and a helper file to help with generating unique ids

---
+++ Develop/public/assets/js/myindex.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const reviews = require('./db/reviews.json');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
